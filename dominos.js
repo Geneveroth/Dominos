3e6780745b4bad827e49b04d76fe49a47a182e62
Sam Black <61167135+Geneveroth@users.noreply.github.com>
2021-07-23 19:53:54 -0700

    Testing Improvements
    -finished the logic of the test
    TODO:
    -fleshing out assertions
    -make sure test runs in its entirety

--- dominos.js
+++ dominos.js
@@ -11,34 +11,69 @@ describe('Dominos Test', () => {
         cy.get('#Service_Type_Carryout').click({force:true})
     })
 
-    it('Enter the zip code and click search', () => {
+    it('Enter the zip code 94611 and click search', () => {
         cy.get('#Postal_Code_Sep').should('be.visible')
             .type('94611').should('have.value', '94611')
         cy.contains("Search Locations").click()
     })
 
-    it('Choose the first result', () => {
-        cy.get('[')
-        //try getting the dom element that shows the error message
-        //do an if check and if it exists, search for future carryout, else store pickup
-        cy.contains('Store Pickup').first().click()
+    it('If a "store closed" error appears, click "Future Carryout Order"', () => {
+        cy.get('[class$="carryout"]')
         cy.contains( 'Future Carryout Order').first().click()
+    })
+    
+    it('If no error appears, click "Store Pickup"', () => {
+        cy.get('[class$="carryout"]')
+        cy.contains('Store Pickup').first().click()
     })
 
     it('Click on "Build Your Own Pizza"', () => {
        cy.contains('Build Your Own Pizza').click() 
     })
 
-    it('Choose pizza size and crust', () => {
+    it('If the "Extra Cheese" dialog box appears, click "No Thanks"', () =>{
+        cy.get('.stepUpSell').should('be.visible')
+        cy.contains('No Thanks').click()
+    })
+
+    it('Choose "X-Large" size pizza and crust', () => {
         cy.get('[id$=16]').click()
         cy.contains('Cheese & Sauce').click()
     })
 
-    it('Choose cheese and sauce and add special cooking instructions', () => {
+    it('Choose "Double" Cheese and "Garlic Parmesan Sauce"', () => {
         cy.get('select[name="Weight|C--1"]').select('Double')
         cy.get('.c-topping-Xw').check()
-        cy.get('button[data-quid="cooking-instructions"]').click()
-        //find proper name for collapsed button
     })
 
+    it('Add special cooking instructions of "Well Done" and "Square Cut"', () => {
+        cy.get('[class^="button--cooking-instructions"]').click()
+        cy.get('[data-description="Well Done"').check({force:true})
+        cy.get('[data-description="Square Cut"').check({force:true})
+    })
+
+    it('Click on "Toppings"', () => {
+        cy.contains('Toppings').click()
+    })
+
+    it('Choose "Pepperoni", "Bacon", and "Spinach"', () =>{
+        cy.get('.topping-P').check()
+        cy.get('.topping-K').check()
+        cy.get('.topping-Si').check()
+    })
+
+    it('Add the pizza to the order', () =>{
+        cy.contains('Add to Order').click()
+    })
+
+    it('Choose "Garlic Dipping Sauce" and click "Add Sides"', () => {
+        cy.get('[value="F_SIDGAR"]').check()
+        cy.contains('Add Sides').click()
+    })
+
+    it('Check the cart for the order and veryify that it is correct', () => {
+        cy.contains('Cart').click({force:true})
+        cy.get('[data-quid="mini-cart-product-toppings-whole"]').should('have.text', "Whole: Double Cheese, Garlic Parmesan Sauce, Pepperoni, Bacon, Spinach, No Robust Inspired Tomato Sauce")
+        cy.get('[data-quid="mini-cart-product-2-name-button"]').should('have.text', "Garlic Dipping Cup")
+    })
   })
